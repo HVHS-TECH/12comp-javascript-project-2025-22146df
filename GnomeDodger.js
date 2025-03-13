e8241b040d8e9944f7c454190b38db408650d958
22146df <22146df@hvhs.school.nz>
2025-03-13 21:58:14 +0000

    making walls finished gnome vert and hori

--- GnomeDodger.js
+++ GnomeDodger.js
@@ -10,10 +10,15 @@ const PLAYER_HEIGHT = 25;
 const PLAYER_WIDTH = 25;
 const PLAYERSIZE = 25;
 const PLAYERSPEED = 5;
-const GNOMESIZE = 200;
+const GNOMESIZE = 20;
+const GNOME_Y = 10;
+const GNOME_X = 10;
+const GNOMESPEED = [5,10,15,20];
+const WALLTHICK = 5;
 
 let gnomex;
 let gnomey;
+
 
 /*******************************************************/
 //SETUP
@@ -23,9 +28,19 @@ function setup() {
     stickman = new Sprite((GAMEWIDTH/2), (GAMEHEIGHT/2), PLAYERSIZE, PLAYERSIZE, 'd');
     stickman.color = 'black';
 
-    let gnomey = (random(1,499));
-    let gnomex = (random(1,499));
-    gnomeMaker();
+    //let gnomey = (random(1,499));
+    //let gnomex = (random(1,499));
+    gnomeMakerH();
+    gnomeMakerV();
+
+    Lwall = new Sprite (0, (GAMEHEIGHT/2), GAMEHEIGHT, 5, 'k')
+    Lwall.color = 'black';
+    Rwall = new Sprite (500, (GAMEHEIGHT/2), 5, 'k')
+    Rwall.color = 'black';
+    Twall = new Sprite ((GAMEWIDTH/2), 0, 5, 'k')
+    Twall.color = 'black';
+    Bwall = new Sprite ((GAMEWIDTH/2), 500, 5, 'k')
+    
 }
 
 /*******************************************************/
@@ -34,6 +49,10 @@ function setup() {
 function draw(){
     background('white');
     movement();
+    gnomeDetectH();
+    gnomeDetectV();
+
+   
 }
 /*******************************************************/
 //FUNCTIONS
@@ -65,9 +84,39 @@ function movement(){
 }
 
 
-function gnomeMaker(){
-    //make the gnome at a random point on the border
-    gnome = new Sprite((0, gnomex), (0, gnomey), GNOMESIZE,'k');
-    gnome.color = 'red';
+function gnomeMakerV(){
+    //make the gnome at a random point on the border at the top
+    let speedV = random(GNOMESPEED); // pick random speed from speed array
+    gnomeV = new Sprite(random(5,495), GNOME_Y, GNOMESIZE,'k');
+    gnomeV.color = 'red';
+    gnomeV.vel.y = speedV;
+
+}
+
+function gnomeMakerH(){
+     //make the gnome at a random point on the border
+     let speedH = random(GNOMESPEED); // pick random speed from speed array
+     gnomeH = new Sprite(GNOME_X, random(5,495), GNOMESIZE,'k');
+     gnomeH.color = 'red';
+     gnomeH.vel.x = speedH;
+}
+
+function gnomeDetectV(){
+  if (gnomeV.y>GAMEHEIGHT){
+    console.log("bottom");
+    gnomeV.remove();
+    gnomeMakerV();
+  } else{
+    console.log("TOP");
+  }
+}
+function gnomeDetectH(){
+  if (gnomeH.x>GAMEWIDTH){
+    console.log("inside");
+    gnomeH.remove();
+    gnomeMakerH();
+  } else{
+    console.log("RIGHTWALL");
+  }
 }
 /*******************************************************/
